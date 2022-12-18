fce5f6d13dd33d8324656ab29a464389812a5b34
Xing Yahao <keidarcy.1015@gmail.com>
2022-12-18 22:25:00 +0900

    feat: day07

--- day07/day07.go
+++ day07/day07.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	input := getInput()
 	part1 := getPart1(input)
-	// part2 := getPart2(input)
+	part2 := getPart2(input)
 	fmt.Println("part1 result: ", part1)
-	// fmt.Println("part2 result: ", part2)
+	fmt.Println("part2 result: ", part2)
 }
 
 type node struct {
@@ -26,26 +28,114 @@ type node struct {
 func getPart1(input string) int {
 	lines := strings.Split(input, "\n")
 
-	// dirs := []*node{}
+	dirs := []*node{}
 	var currentDir *node
 
 	for _, line := range lines {
-		commands := strings.Split(line, " ")
+		command := strings.Split(line, " ")
 
 		// $ cd a
-		if len(commands) > 2 {
-			if commands[2] == ".." {
+		if len(command) > 2 {
+			if command[2] == ".." {
 				currentDir = currentDir.parent
-			} else if commands[2] == "/" {
+			} else if command[2] == "/" {
 				currentDir = &node{"/", 0, false, make(map[string]*node), nil}
 			} else {
-				currentDir = currentDir.children[commands[2]]
+				currentDir = currentDir.children[command[2]]
 			}
 		}
 
+		if rune(command[0][0]) == '$' {
+			// $ ls
+			continue
+		} else if rune(command[0][0]) == 'd' {
+			// dir a
+			currentDir.children[command[1]] = &node{command[1], 0, false, make(map[string]*node), currentDir}
+			dirs = append(dirs, currentDir.children[command[1]])
+		} else {
+			// 2557 g
+			size, _ := strconv.Atoi(command[0])
+			currentDir.children[command[1]] = &node{command[1], size, true, nil, currentDir}
+		}
 	}
 
-	return 1
+	result := 0
+	for _, dir := range dirs {
+		size := countSize(*dir)
+		if size <= 100000 {
+			result += size
+		}
+	}
+	return result
+}
+
+func getPart2(input string) {
+	lines := strings.Split(input, "\n")
+
+	dirs := []*node{}
+	var currentDir *node
+
+	for _, line := range lines {
+		command := strings.Split(line, " ")
+
+		// $ cd a
+		if len(command) > 2 {
+			if command[2] == ".." {
+				currentDir = currentDir.parent
+			} else if command[2] == "/" {
+				currentDir = &node{"/", 0, false, make(map[string]*node), nil}
+				dirs = append(dirs, currentDir)
+			} else {
+				currentDir = currentDir.children[command[2]]
+			}
+		}
+
+		if rune(command[0][0]) == '$' {
+			// $ ls
+			continue
+		} else if rune(command[0][0]) == 'd' {
+			// dir a
+			currentDir.children[command[1]] = &node{command[1], 0, false, make(map[string]*node), currentDir}
+			dirs = append(dirs, currentDir.children[command[1]])
+		} else {
+			// 2557 g
+			size, _ := strconv.Atoi(command[0])
+			currentDir.children[command[1]] = &node{command[1], size, true, nil, currentDir}
+		}
+	}
+
+	totalSize := 0
+	for _, dir := range dirs {
+		size := countSize(*dir)
+		dir.size = size
+		if size > totalSize {
+			totalSize = size
+		}
+	}
+	targetSize := 30000000 - (70000000 - totalSize)
+	sort.Slice(dirs, func(i, j int) bool {
+		return dirs[i].size < dirs[j].size
+	})
+
+	for _, dir := range dirs {
+		if dir.size > targetSize {
+			return dir.name
+		}
+
+	}
+	return "NO NAME"
+}
+
+func countSize(n node) int {
+
+	if n.isFile {
+		return n.size
+	}
+	size := 0
+	for _, childNode := range n.children {
+		size += countSize(*childNode)
+	}
+	return size
 }
 func getInput() string {
 	input, err := os.ReadFile("./input.txt")
